96299f228cca44542cbddaa0c58fb3cd80436964
ikechukwu-unegbu <iunegbu94@yahoo.com>
2023-01-31 22:41:51 +0100

    M

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a Reddit subreddit."""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of top ten hot posts on a subreddit."""
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 "
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
     }
     params = {
         "limit": 10
@@ -19,4 +19,3 @@ def top_ten(subreddit):
         return
     results = response.json().get("data")
     [print(c.get("data").get("title")) for c in results.get("children")]
-
